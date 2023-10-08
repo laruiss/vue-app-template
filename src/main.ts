afd2b1eea6f7ce029daefe270b7dc1421cc8224d
Stanislas Ormières <stan@stormier.ninja>
2023-10-08 20:34:39 +0200

    feat: :sparkles: configure ssg

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { ViteSSG } from 'vite-ssg'
 
 import App from './App.vue'
-import router from './router'
+import { routes } from './router'
 
 import 'uno.css'
 import './assets/main.css'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+    app.use(createPinia())
+  },
+)
